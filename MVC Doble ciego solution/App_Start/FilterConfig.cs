0340a3839321b2efe1fe4d897bdae0f5e9dda99a
Angel Juarez <ajjjr0827@hotmail.com>
2022-03-14 18:05:36 -0700

    First Commit

---
+++ MVC Doble ciego solution/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Doble_ciego_solution
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MVC Doble ciego solution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Doble_ciego_solution.MvcApplication" Language="C#" %>
