f6fd9fc1e5039ecd7340fc44f8b68e79731351a2
AntonBronnfjell <71841746+AntonBronnfjell@users.noreply.github.com>
2022-05-13 17:35:26 -0700

    Added design...

---
+++ MVC Doble ciego solution/Controllers/DocumentsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Doble_ciego_solution.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        // GET: Documents
+        public ActionResult Documents()
+        {
+            return View();
+        }
+    }
+}
